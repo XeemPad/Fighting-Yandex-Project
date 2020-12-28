7d3fd7e4f7f60f884aad7a9c304a0f1c390ff80b
Эмиль <gabdulvaleev.emil@mail.ru>
2020-12-28 13:56:39 +0300

    Added window in main.py(minor commit)

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 # This file contains code of game menu
+import pygame
 
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
+FPS = 40
+
+
+def terminate():
+    pygame.quit()
+    exit()
+
+
+pygame.init()
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Fighting')
+pygame.display.set_icon(pygame.image.load('data/icon.ico'))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()

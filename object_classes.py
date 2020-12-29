15c234c11c9844bd41095a003436c7fb0efdaafa
Эмиль <gabdulvaleev.emil@mail.ru>
2020-12-29 13:25:05 +0300

    Added reaction of button to mouse pointing on it

--- object_classes.py
+++ object_classes.py
@@ -18,10 +18,14 @@ class Button:
                  width=None, height=None, function=None):
         self.position = 0, 0
         self.position_set = False
+
+        self.text_surface = text_surface
+        self.button_color = button_color
+        self.secondary_button_color = secondary_button_color
         self.function = function
 
-        self.text_width, self.text_height = (text_surface.get_rect().width,
-                                             text_surface.get_rect().height)
+        self.text_width, self.text_height = (self.text_surface.get_rect().width,
+                                             self.text_surface.get_rect().height)
         if width is None:
             width = self.text_width + HORIZONTAL_INDENT
         if height is None:
@@ -30,11 +34,12 @@ class Button:
             raise SizeError('Введённый размер кнопки меньше размера текста')
         self.width, self.height = width, height
         self.surface = pygame.Surface((self.width, self.height))  # Поверхность кнопки
-        self.surface.fill(button_color)  # Основной цвет кнопки
-        pygame.draw.rect(self.surface, secondary_button_color,
+        self.surface.fill(self.button_color)  # Основной цвет кнопки
+        pygame.draw.rect(self.surface, self.secondary_button_color,
                          (0, 0, self.width, self.height), 1)  # Дополнительный цвет(обводка)
-        self.surface.blit(text_surface, ((self.width - self.text_width) // 2,
-                                         (self.height - self.text_height) // 2))  # Наложение текста
+        # Наложение текста на поверхность кнопки:
+        self.surface.blit(self.text_surface, ((self.width - self.text_width) // 2,
+                                              (self.height - self.text_height) // 2))
 
     def get_pos(self):
         return self.position
@@ -54,8 +59,25 @@ class Button:
     def trigger(self):
         self.function()
 
-    def check_click_position(self, mouse_pos):
+    def check_mouse_position(self, mouse_pos):
         if ((self.position[0] <= mouse_pos[0] <= (self.position[0] + self.width) and
              self.position[1] <= mouse_pos[1] <= (self.position[1] + self.height))):
-            # Клик был по данной кнопке
-            self.trigger()
+            # Мышь наведена на данную кнопку
+            return True
+        return False
+
+    def set_under_mouse_effect(self, mouse_is_on_btn=True):  # Реакция кнопки на наведение мыши
+        if mouse_is_on_btn:
+            # Перерисовка кнопки с обратными цветами:
+            self.surface.fill(self.secondary_button_color)
+            pygame.draw.rect(self.surface, self.button_color, (0, 0, self.width, self.height), 1)
+            self.surface.blit(self.text_surface, ((self.width - self.text_width) // 2,
+                                                  (self.height - self.text_height) // 2))
+        else:
+            # Отрисовка кнопки со стандартными цветами
+            self.surface = pygame.Surface((self.width, self.height))
+            self.surface.fill(self.button_color)
+            pygame.draw.rect(self.surface, self.secondary_button_color,
+                             (0, 0, self.width, self.height), 1)
+            self.surface.blit(self.text_surface, ((self.width - self.text_width) // 2,
+                                                  (self.height - self.text_height) // 2))
